b80927e4bbecf8ac80f285b126e967c0e4110b52
gondowan <dsalazar446@gmail.com>
2025-10-22 21:27:49 -0400

    final config

--- src/lib/Common/infrastructure/config/config.ts
+++ src/lib/Common/infrastructure/config/config.ts
@@ -17,19 +17,44 @@ if (fs.existsSync(secretsPath)) {
   );
 }
 
+const PARAMS = {
+  FIREBASE_STORAGE_BUCKET: defineString("FIREBASE_STORAGE_BUCKET"),
+  FIREBASE_PROJECT_ID: defineString("FIREBASE_PROJECT_ID"),
+  JWT_SECRET: defineString("JWT_SECRET"),
+  JWT_EXPIRES_IN: defineString("JWT_EXPIRES_IN"),
+  NODE_ENV: defineString("NODE_ENV"),
+  PORT: defineInt("PORT"),
+  PREFIX: defineString("PREFIX"),
+};
 export const config = {
   firebase: {
-    bucket : process.env.FIREBASE_STORAGE_BUCKET || defineString("FIREBASE_STORAGE_BUCKET").value() || "",
+    bucket:
+      process.env.FIREBASE_STORAGE_BUCKET ||
+      tryGet(() => PARAMS.FIREBASE_STORAGE_BUCKET.value(), ""),
     firebaseKeyPath: process.env.FIREBASE_KEY_PATH || "",
-    projectId: process.env.FIREBASE_PROJECT_ID || defineString("FIREBASE_PROJECT_ID").value() || "",
+    projectId:
+      process.env.FIREBASE_PROJECT_ID ||
+      tryGet(() => PARAMS.FIREBASE_PROJECT_ID.value(), ""),
   },
   jwt: {
-    secret: process.env.JWT_SECRET || defineString("JWT_SECRET").value() ||"",
-    expireIn: process.env.JWT_EXPIRES_IN || defineString("JWT_EXPIRES_IN").value()|| "1h",
+    secret:
+      process.env.JWT_SECRET || tryGet(() => PARAMS.JWT_SECRET.value(), ""),
+    expireIn:
+      process.env.JWT_EXPIRES_IN ||
+      tryGet(() => PARAMS.JWT_EXPIRES_IN.value(), "1h"),
   },
-  port: process.env.PORT || defineInt("PORT").value() ||4000,
-  prefix: process.env.PREFIX || defineString("PREFIX").value() ||"/api",
-  production: process.env.NODE_ENV === "production" || defineString("NODE_ENV").value() === "production",
-
-  
+  port: process.env.PORT || tryGet(() => PARAMS.PORT.value(), 4000),
+  prefix: process.env.PREFIX || tryGet(() => PARAMS.PREFIX.value(), "/api"),
+  production:
+    process.env.NODE_ENV === "production" ||
+    tryGet(() => PARAMS.NODE_ENV.value(), "") === "production",
 };
+
+
+function tryGet<T>(getter: () => T, fallback: T): T {
+  try {
+    return getter() ?? fallback;
+  } catch {
+    return fallback;
+  }
+}
